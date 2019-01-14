4e930863e5fee93becaeb111de0b231e4ee00444
Viktor <33760375+matkun81@users.noreply.github.com>
2019-01-14 20:28:49 +0300

    lombook

--- src/main/java/com/by/Viktor/Controller/AdminController.java
+++ src/main/java/com/by/Viktor/Controller/AdminController.java
@@ -1,20 +1,14 @@
 package com.by.Viktor.Controller;
 
 import com.by.Viktor.Model.Message;
-import com.by.Viktor.Model.Role;
-import com.by.Viktor.Model.User;
 import com.by.Viktor.Repository.MessageRepository;
-import com.by.Viktor.Repository.UserRepository;
-import com.fasterxml.jackson.databind.ser.std.MapSerializer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
